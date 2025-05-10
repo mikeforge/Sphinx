310c73ed18d727a4774e307ebf5e82540404b5db
mikeforge <mike.forge@gmail.com>
2025-05-10 18:38:33 -0400

    Init repo

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return <header><h1>My Company</h1></header>;
+};
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+const Home: React.FC = () => {
+  return <div>Welcome to the domain of the Sphinx!</div>;
+};
+
+export default Home;
